29062fd851b6103ba549352232b214ededda6dcd
bhumichouhan <111154255+bhumichouhan@users.noreply.github.com>
2022-08-12 15:58:50 -0500

    Adding my first few assignments program

---
+++ Emp.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Emp {
+    public  static void main(String[] args)
+    {
+        int empno= 100;
+        String empName= "Anika";
+        boolean employeed=true;
+        System.out.println("employee details--");
+        System.out.println(empno);
+        System.out.println(empName);
+        System.out.println(employeed);
+
+    }
+}
